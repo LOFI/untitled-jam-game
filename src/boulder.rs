1c53fdd2e9fd04d4b30c97695f94e1022c13c6e6
Kenneth Love <11908+kennethlove@users.noreply.github.com>
2024-03-20 17:19:48 -0700

    Textures (#24)
    * Added ground and boulder textures. Addresses #16 and #17
    
    * new graphic
    
    * Ignore .DS_Store files
    
    * ground plane
    
    * Added foreground
    
    * Moved textures to folder

--- src/boulder.rs
+++ src/boulder.rs
@@ -1,4 +1,4 @@
-use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
+use bevy::{asset, prelude::*, sprite::MaterialMesh2dBundle};
 use bevy_rapier2d::{plugin::systems::RigidBodyWritebackComponents, prelude::*};
 
 pub struct BoulderPlugin;
@@ -23,7 +23,6 @@ fn freeze_boulder(mut commands: Commands, mut boulder: Query<Entity, With<Boulde
     }
 
     commands.entity(boulder.single()).insert(RigidBody::Fixed);
-
 }
 
 fn unfreeze_boulder(mut commands: Commands, mut boulder: Query<Entity, With<Boulder>>) {
@@ -32,18 +31,18 @@ fn unfreeze_boulder(mut commands: Commands, mut boulder: Query<Entity, With<Boul
     }
 
     commands.entity(boulder.single()).insert(RigidBody::Dynamic);
-
 }
 
 fn spawn_boulder(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
+    asset_server: Res<AssetServer>,
 ) {
     commands
         .spawn(MaterialMesh2dBundle {
             mesh: meshes.add(Circle { radius: 64. }).into(),
-            material: materials.add(Color::GRAY),
+            material: materials.add(asset_server.load("textures/dirt.png")),
             transform: Transform::from_xyz(0.0, 0.0, 0.0),
             ..default()
         })
--- src/ground.rs
+++ src/ground.rs
@@ -1,24 +1,33 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
 use bevy_rapier2d::prelude::*;
 
-use crate::{GameState, WINDOW_BOTTOM_Y, WINDOW_HEIGHT, WINDOW_WIDTH};
+use crate::{player::Player, GameState, WINDOW_BOTTOM_Y, WINDOW_HEIGHT, WINDOW_WIDTH};
 
-const COLOR_FLOOR: Color = Color::GREEN;
+const COLOR_FLOOR: Color = Color::DARK_GREEN;
 
 pub struct GroundPlugin;
 
 #[derive(Component)]
 struct Ground;
 
+#[derive(Component)]
+struct Foreground;
+
 impl Plugin for GroundPlugin {
     fn build(&self, app: &mut App) {
-        app
-            .add_systems(OnExit(GameState::MainMenu), spawn_floor)
-            .add_systems(FixedUpdate, expand.run_if(in_state(GameState::InGame)));
+        app.add_systems(OnEnter(GameState::InGame), (spawn_foreground, spawn_ground))
+            .add_systems(
+                FixedUpdate,
+                (expand, keep_centered).run_if(in_state(GameState::InGame)),
+            );
     }
 }
 
-fn spawn_floor(mut commands: Commands) {
+fn spawn_foreground(mut commands: Commands, ground_query: Query<&Ground>) {
+    if !ground_query.is_empty() {
+        return;
+    }
+
     // Slope
     commands
         .spawn(SpriteBundle {
@@ -27,15 +36,38 @@ fn spawn_floor(mut commands: Commands) {
                 ..default()
             },
             transform: Transform {
-                translation: Vec3::new(0., WINDOW_BOTTOM_Y, 0.),
+                translation: Vec3::new(0., WINDOW_BOTTOM_Y - WINDOW_HEIGHT / 4. - 12., 0.),
                 scale: Vec3::new(WINDOW_WIDTH * 2., WINDOW_HEIGHT / 2., 1.),
                 rotation: Quat::from_rotation_z(7.5_f32.to_radians()),
             },
             ..default()
         })
+        .insert(Foreground);
+}
+
+fn spawn_ground(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    commands
+        .spawn(MaterialMesh2dBundle {
+            mesh: meshes
+                .add(Plane3d {
+                    normal: Direction3d::Y,
+                })
+                .into(),
+            material: materials.add(Color::DARK_GREEN),
+            transform: Transform {
+                translation: Vec3::new(0., WINDOW_BOTTOM_Y, 0.),
+                rotation: Quat::from_rotation_z(7.5_f32.to_radians()),
+                ..default()
+            },
+            ..default()
+        })
         .insert(Ground)
-        .insert(RigidBody::KinematicPositionBased)
-        .insert(Collider::cuboid(0.5, 0.5));
+        .insert(RigidBody::Fixed)
+        .insert(Collider::cuboid(WINDOW_WIDTH, 1.));
 }
 
 fn expand(
@@ -48,8 +80,24 @@ fn expand(
     let player = player_query.single();
     let mut ground = query.single_mut();
 
-    if player.desired_translation.x != 0. {
-        ground.scale.x += 1.;
-    }
+    ground.scale.x += player.effective_translation.x.abs();
+}
 
+fn keep_centered(
+    mut query: Query<(&mut Transform, &Foreground), Without<Player>>,
+    player_query: Query<
+        (&Transform, &KinematicCharacterControllerOutput),
+        With<crate::player::Player>,
+    >,
+) {
+    if player_query.is_empty() || query.is_empty() {
+        return;
+    }
+    let (player, output) = player_query.single();
+    let (mut ground, _) = query.single_mut();
+
+    ground.translation.x = player.translation.x;
+    if output.grounded {
+        ground.translation.y = player.translation.y - WINDOW_HEIGHT / 4. - 24.;
+    }
 }
