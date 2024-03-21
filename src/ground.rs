8b4c751af29e361e55a893c7e27d1d9cd8cf595e
Kenneth Love <kennethlove@gmail.com>
2024-03-20 17:16:35 -0700

    Added foreground

--- src/ground.rs
+++ src/ground.rs
@@ -1,24 +1,29 @@
-use bevy::{prelude::*, render::mesh::shape::Plane, sprite::MaterialMesh2dBundle};
+use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
 use bevy_rapier2d::prelude::*;
 
-use crate::{GameState, WINDOW_BOTTOM_Y, WINDOW_HEIGHT, WINDOW_WIDTH};
+use crate::{player::Player, GameState, WINDOW_BOTTOM_Y, WINDOW_HEIGHT, WINDOW_WIDTH};
 
-const COLOR_FLOOR: Color = Color::GREEN;
+const COLOR_FLOOR: Color = Color::DARK_GREEN;
 
 pub struct GroundPlugin;
 
 #[derive(Component)]
 struct Ground;
 
+#[derive(Component)]
+struct Foreground;
+
 impl Plugin for GroundPlugin {
     fn build(&self, app: &mut App) {
-        app
-            .add_systems(OnEnter(GameState::InGame), spawn_ground)
-            .add_systems(FixedUpdate, expand.run_if(in_state(GameState::InGame)));
+        app.add_systems(OnEnter(GameState::InGame), (spawn_foreground, spawn_ground))
+            .add_systems(
+                FixedUpdate,
+                (expand, keep_centered).run_if(in_state(GameState::InGame)),
+            );
     }
 }
 
-fn spawn_floor(mut commands: Commands, asset_server: Res<AssetServer>, ground_query: Query<&Ground>) {
+fn spawn_foreground(mut commands: Commands, ground_query: Query<&Ground>) {
     if !ground_query.is_empty() {
         return;
     }
@@ -31,26 +36,27 @@ fn spawn_floor(mut commands: Commands, asset_server: Res<AssetServer>, ground_qu
                 ..default()
             },
             transform: Transform {
-                translation: Vec3::new(0., WINDOW_BOTTOM_Y, 0.),
+                translation: Vec3::new(0., WINDOW_BOTTOM_Y - WINDOW_HEIGHT / 4. - 12., 0.),
                 scale: Vec3::new(WINDOW_WIDTH * 2., WINDOW_HEIGHT / 2., 1.),
                 rotation: Quat::from_rotation_z(7.5_f32.to_radians()),
             },
             ..default()
         })
-        .insert(Ground)
-        .insert(RigidBody::KinematicPositionBased)
-        .insert(Collider::cuboid(0.5, 0.5));
+        .insert(Foreground);
 }
 
 fn spawn_ground(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
-    asset_server: Res<AssetServer>,
 ) {
     commands
         .spawn(MaterialMesh2dBundle {
-            mesh: meshes.add(Plane3d { normal: Direction3d::Y }).into(),
+            mesh: meshes
+                .add(Plane3d {
+                    normal: Direction3d::Y,
+                })
+                .into(),
             material: materials.add(Color::DARK_GREEN),
             transform: Transform {
                 translation: Vec3::new(0., WINDOW_BOTTOM_Y, 0.),
@@ -61,7 +67,7 @@ fn spawn_ground(
         })
         .insert(Ground)
         .insert(RigidBody::Fixed)
-        .insert(Collider::cuboid(WINDOW_WIDTH, 5.));
+        .insert(Collider::cuboid(WINDOW_WIDTH, 1.));
 }
 
 fn expand(
@@ -74,8 +80,24 @@ fn expand(
     let player = player_query.single();
     let mut ground = query.single_mut();
 
-    if player.desired_translation.x != 0. {
-        ground.scale.x += 1.;
-    }
+    ground.scale.x += player.effective_translation.x.abs();
+}
 
+fn keep_centered(
+    mut query: Query<(&mut Transform, &Foreground), Without<Player>>,
+    player_query: Query<
+        (&Transform, &KinematicCharacterControllerOutput),
+        With<crate::player::Player>,
+    >,
+) {
+    if player_query.is_empty() || query.is_empty() {
+        return;
+    }
+    let (player, output) = player_query.single();
+    let (mut ground, _) = query.single_mut();
+
+    ground.translation.x = player.translation.x;
+    if output.grounded {
+        ground.translation.y = player.translation.y - WINDOW_HEIGHT / 4. - 24.;
+    }
 }
